252d5b52e4466c2507af2354083b6e114225c8d8
mkwak <minjunkwak@gmail.com>
2021-05-24 11:53:42 -0600

    asdf

--- server/index.js
+++ server/index.js
@@ -2,3 +2,10 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import express from 'express';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+
